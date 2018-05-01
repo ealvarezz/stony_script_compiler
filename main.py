0c5b5e6d5755469138ce80c1567f71de05fb8a83
ealvarezz <edy.alva_101@hotmail.com>
2018-05-01 00:07:53 -0400

    adding statements to the lexer

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # Edwin Alvarez 109448839
 # First I will define the classes that my parser is going to use
+
+variables = {}
+
 class Node:
     def __init__(self):
         print("init node")
@@ -10,22 +13,31 @@ class Node:
     def execute(self):
         return 0
 
+
 class RootNode(Node):       # This is the root note that will perform the
     def __init__(self, v):  # evalaluation from the top down once the tree
         self.v = v      # the tree if finally constructed
 
     def execute(self):
-        val = self.v.evaluate()
+        self.v.evaluate()
 
-        if isinstance(val, str):
-            print('\'' + val + '\'')
-        else:
-            print(val)
+
+
+class StmtNode(Node):
+    def __init__(self, stmt, stmt_list):
+        self.stmt = stmt
+        self.stmt_list = stmt_list
+
+    def evaluate(self):
+        self.stmt.evaluate()
+        if self.stmt_list != None:
+            self.stmt_list.evaluate()
+
 
 
 class NumberNode(Node):
     def __init__(self, v):
-        if('.' in v):
+        if '.' in v:
             self.v = float(v)
         else:
             self.v = int(v)
@@ -34,12 +46,30 @@ class NumberNode(Node):
         return self.v
 
 
+class BlockNode(Node):
+    def __init__(self, v):
+        self.v = v
+
+    def evaluate(self):
+        self.evaluate()
+
+
 class StringNode(Node):
     def __init__(self, v):
         self.v = v
 
     def evaluate(self):
         return self.v
+
+
+
+class NameNode(Node):
+    def __init__(self, v):
+        self.v = v
+
+    def evaluate(self):
+        return self.v
+
 
 
 class BooleanNode(Node):
@@ -51,6 +81,7 @@ class BooleanNode(Node):
 
     def evaluate(self):
         return self.v
+
 
 
 class BinopNode(Node):
@@ -116,6 +147,140 @@ class BoolopNode(Node):
             elif (self.op == '<>'):
                 return self.v1.evaluate() != self.v2.evaluate()
 
+        except:
+            print('SEMANTIC ERROR')
+            raise Exception
+
+
+class PrintNode(Node):
+    def __init__(self, v):
+        self.v = v
+
+    def evaluate(self):
+        try:
+            print(self.v.evaluate())
+        except:
+            print('SEMANTIC ERROR')
+            raise Exception
+
+
+
+
+class AssignNode(Node):
+    def __init__(self, name, v):
+        self.name = name
+        self.v = v
+
+    def evaluate(self):
+        try:
+            variables[self.name.evaluate()] = self.v.evaluate()
+        except:
+            print('SEMANTIC ERROR')
+            raise Exception
+
+
+
+class AssignVar2VarNode(Node):
+    def __init__(self, name1, name2):
+        self.name1 = name1
+        self.name2 = name2
+
+    def evaluate(self):
+        try:
+            variables[self.name1.evaluate()] = variables[self.name2.evaluate()]
+        except:
+            print('SEMANTIC ERROR')
+            raise Exception
+
+
+
+class WhileNode(Node):
+    def __init__(self, condition, stmt_list):
+        self.condition = condition
+        self.stmt_list = stmt_list
+
+    def evaluate(self):
+        try:
+            while self.condition.evaluate():
+                self.stmt_list.evaluate()
+
+        except:
+            print('SEMANTIC ERROR')
+            raise Exception
+
+
+
+class IfNode(Node):
+    def __init__(self, condition, stmt_list):
+        self.condition = condition
+        self.stmt_list = stmt_list
+
+    def evaluate(self):
+        try:
+            if self.condition.evaluate():
+                self.stmt_list.evaluate()
+        except:
+            print('SEMANTIC ERROR')
+            raise Exception
+
+
+
+class IfElseNode(Node):
+    def __init__(self, condition, stmt_list1, stmt_list2):
+        self.condition = condition
+        self.stmt_list1 = stmt_list1
+        self.stmt_list2 = stmt_list2
+
+    def evaluate(self):
+        try:
+            if self.condition.evaluate():
+                self.stmt_list1.evaluate()
+            else:
+                self.stmt_list2.evaluate()
+        except:
+            print('SEMANTIC ERROR')
+            raise Exception
+
+
+
+class ListVarIndexNode(Node):
+    def __init__(self, list_name, index):
+        self.list_name = list_name
+        self.index = index
+
+    def evaluate(self):
+        try:
+            return variables[self.list_name.evaluate()][self.index.evaluate()]
+        except:
+            print('SEMANTIC ERROR')
+            raise Exception
+
+
+
+class ListVarAssignNode(Node):
+    def __init__(self, list_name, index, v):
+        self.list_name = list_name
+        self.index = index
+        self.v = v
+
+    def evaluate(self):
+        try:
+            variables[self.list_name.evaluate()][self.index.evaluate()] = self.v
+        except:
+            print('SEMANTIC ERROR')
+            raise Exception
+
+
+
+class ListAssignNode(Node):
+    def __init__(self, expr, index, v):
+        self.expr = expr
+        self.index = index
+        self.v = v
+
+    def evaluate(self):
+        try:
+            self.expr.evaluate()[self.index.evaluate()] = self.v
         except:
             print('SEMANTIC ERROR')
             raise Exception
@@ -194,19 +359,21 @@ class ListItemNode(Node):
             raise Exception
 
 tokens = (
-    'LBRACE', 'RBRACE',
+    'LBRACKET', 'RBRACKET',
     'COMMA','LPAREN', 'RPAREN',
     'NUMBER',
     'PLUS','MINUS','TIMES','DIVIDE',
     'AND', 'OR', 'POW', 'MOD', 'NOT', 'FLOOR',
-    'GT', 'GE', 'EQ', 'NE', 'LT', 'LE', 'STRING', 'BOOL','IN'
+    'GT', 'GE', 'EQ', 'NE', 'LT', 'LE', 'STRING', 'BOOL','IN',
+    'IF', 'WHILE', 'LBRACE', 'RBRACE', 'ELSE', 'VARNAME', 'SEMI',
+    'ASSIGN', 'PRINT'
     )
 
 # Tokens
 t_LPAREN  = r'\('
 t_RPAREN  = r'\)'
-t_RBRACE = r'\]'
-t_LBRACE = r'\['
+t_RBRACKET = r'\]'
+t_LBRACKET = r'\['
 t_COMMA = r','
 t_PLUS    = r'\+'
 t_MINUS   = r'-'
@@ -225,11 +392,30 @@ t_LE = r'\<\='
 t_NE = r'\<\>'
 t_FLOOR = r'\/\/'
 t_IN = r'in'
+t_IF = r'if'
+t_WHILE = r'while'
+t_LBRACE = r'\{'
+t_RBRACE = r'\}'
+t_ELSE = r'else'
+t_SEMI = r'\;'
+t_ASSIGN = r'\='
+t_PRINT = r'print'
+
 
 def t_BOOL(t):
     r'True|False'
     try:
         t.value = BooleanNode(t.value)
+    except ValueError:
+        print("Something went wrong with string  %s", t.value)
+        t.value = ''
+    return t
+
+
+def t_VARNAME(t):
+    r'[A-Za-z][A-Za-z0-9_]*'
+    try:
+        t.value = NameNode(t.value)
     except ValueError:
         print("Something went wrong with string  %s", t.value)
         t.value = ''
@@ -284,16 +470,105 @@ precedence = (
     ('left', 'MOD'),
     ('left', 'TIMES', 'DIVIDE'),
     ('right', 'POW'),
-    ('left', 'LBRACE')
+    ('left', 'LBRACKET')
 #    ('left', 'LPAREN', 'RPAREN')
 )
 
 
 def p_root(t):
     '''
-    root : expr
+    root : LBRACE stmt_list RBRACE
     '''
-    t[0] = RootNode(t[1])
+    t[0] = RootNode(t[2])
+
+
+def p_block_statement(t):
+    '''
+    stmt : LBRACE stmt_list RBRACE
+    '''
+    t[0] = BlockNode(t[2])
+
+
+def p_statement_list(t):
+    '''
+    stmt_list : stmt stmt_list
+    '''
+    t[0] = StmtNode(t[1], t[2])
+
+
+def p_statement_list_empty(t):
+    '''
+    stmt_list :
+    '''
+    
+    t[0] = None
+
+
+def p_print_statement(t):
+    '''
+    stmt : PRINT LPAREN expr  RPAREN SEMI
+    '''
+    t[0] = PrintNode(t[3])
+
+
+def p_assign_statement(t):
+    '''
+    stmt : VARNAME ASSIGN expr SEMI
+    '''
+    t[0] = AssignNode(t[1], t[3])
+
+
+def p_assign_var_to_var_statement(t):
+    '''
+    stmt : VARNAME ASSIGN VARNAME SEMI
+    '''
+    t[0] = AssignVar2VarNode(t[1], t[3])
+
+
+def p_assign_var_list(t):
+    '''
+    stmt : VARNAME LBRACKET expr RBRACKET ASSIGN expr SEMI
+    '''
+    t[0] = ListVarAssignNode(t[1], t[3], t[6])
+
+
+def p_assign_list_statement(t):
+    '''
+    stmt : expr LBRACKET expr RBRACKET ASSIGN expr SEMI
+         | STRING  LBRACKET expr RBRACKET ASSIGN expr SEMI
+    '''
+    t[0] = ListAssignNode(t[1], t[3], t[6])
+
+
+def p_while_statement(t):
+    '''
+    stmt : WHILE LPAREN expr RPAREN LBRACE stmt_list  RBRACE
+    '''
+    t[0] = WhileNode(t[3], t[6])
+
+
+
+def p_if_statement(t):
+    '''
+     stmt : IF LPAREN expr RPAREN LBRACE stmt_list RBRACE
+    '''
+    t[0] = IfNode(t[3], t[6])
+
+
+
+def p_if_else_statement(t):
+    '''
+    stmt : IF LPAREN expr RPAREN LBRACE stmt_list RBRACE ELSE LBRACE stmt_list RBRACE
+    '''
+    t[0] = IfElseNode(t[3], t[6], t[10])
+
+
+
+def p_name_expression_index(t):
+    '''
+    expr : VARNAME LBRACKET expr RBRACKET
+    '''
+    t[0] = ListVarIndexNode(t[1], t[3])
 
 
 def p_expression_binop(t):
@@ -325,7 +600,7 @@ def p_expression_bincomp(t):
 
 def p_expression_list(t):
     '''
-    expr : LBRACE items RBRACE
+    expr : LBRACKET items RBRACKET
     '''
     t[0] = t[2]
 
@@ -347,15 +622,15 @@ def p_expression_last_item(t):
 
 def p_expression_index(t):
     '''
-    expr : expr LBRACE expr RBRACE
-         | STRING LBRACE expr RBRACE
+    expr : expr LBRACKET expr RBRACKET
+         | STRING LBRACKET expr RBRACKET
     '''
     t[0] = ListIndex(t[1], t[3])
 
 
 def p_expression_empty_list(t):
     '''
-    expr : LBRACE RBRACE
+    expr : LBRACKET RBRACKET
     '''
     t[0] = ListNode(None)
 
@@ -417,20 +692,26 @@ if (len(sys.argv) != 2):
 fd = open(sys.argv[1], 'r')
 code = ""
 for line in fd:
-    yacc.yacc()
-    code = line.strip()
+    code += line
+
+yacc.yacc()
+code = line.strip()
 #    print(code)
 
-    try:
-        lex.input(code)
-        while True:
-            token = lex.token()
-            if not token: break
-#            print(token)
-        ast = yacc.parse(code)
-        ast.execute()
-    except Exception:
-        pass
+
+code = code.replace("\n", "")
+code = code.replace("\t", "")
+
+try:
+    lex.input(code)
+    while True:
+        token = lex.token()
+        if not token: break
+#           print(token)
+    ast = yacc.parse(code)
+    ast.execute()
+except Exception:
+    pass
        # print("ERROR")
 #        raise Exception
 
